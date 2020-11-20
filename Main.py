2e7cb9e33496c7ae7f882b236d6d14c638f6b35c
Oscar Quesada <oscaardanielqc@outlook.es>
2020-11-20 08:10:54 -0600

    Commit 20/11/2020 8am

--- Main.py
+++ Main.py
@@ -1,39 +1,55 @@
 #Proyecto 2 de Estructuras  de Datos
 # Por: Oscar Quesada
-
+import re
 varTable= dict()
 typeVar = {'void':0,'int':0,'float':0,'string':0}
-symbolTable = {'if':0,'while':0,'(':0,')':0,'{':0,'}':0,'=':0,';':0,'+':0,'-':0,'<':0,'>':0,'*':0,'/':0,',':0}
+symbolTable = {'return':0, 'if':0,'while':0,'(':0,')':0,'{':0,'}':0,'=':0,';':0,'+':0,'-':0,'<':0,'>':0,'*':0,'/':0,',':0}
+
 
 
 def printElement(element):
     print
     element
 def OpenFile():
-    linea = 1
+    numline = 0
     anterior = ""
+    posterior = ""
+    currentFunc = ""
     file_object = open("Mycode.txt", "r")
     if file_object.mode =="r":
     # reading each line
-        for line in file_object:
-            linea + 1
-        # reading each word
-            for word in line.split():
+        try:
+            for line in file_object:
+
+                li = list(line.split())
+
+                numline += 1
+                for word in line.split():
+                    pos = li.index(word)
+
+                #print(pos)
+                    if anterior != None and posterior != None:
+                        anterior = li[pos-1]
+                        posterior = li[pos+1]
                 # verifica si la palabra es numero entero, flotante o string
-                if word.isnumeric() or VerifyFloat(word) or VerifyString(word):
-                    anterior = word
+                    if isFunction(anterior,posterior):
+                        varTable[word] = {'Tipo':'Metodo','Retorno':anterior,'Alcance':'Global'}
                 #si la palabra anterior es un tipo de variable o la palabra actual ya fue declarada
-                elif anterior in typeVar or word in varTable:
-                    varTable[word] = 1
-              #si la parabra es un tipo de variable
-                elif word in typeVar:
-                    typeVar[word] = typeVar[word] + 1
-               #si la palabra es algún simbolo
-                elif word in symbolTable:
-                    symbolTable[word] = symbolTable[word] + 1
-                else:
-                    print("Error en la linea: "+str(linea)+" la variable "+word+" no fue declarada")
-                anterior = word
+                    elif isVar(word) or word in typeVar:
+                        return True
+                    elif anterior in typeVar or word in varTable:
+                        insertVar(word,anterior)
+                #si la palabra es algún simbolo
+                    elif word in symbolTable:
+                        symbolTable[word] = symbolTable[word] + 1
+                    elif anterior == "return":
+                        VerifyReturn(currentFunc,word)
+
+                    else:
+                        print("Error en la linea: "+str(numline)+" la variable "+word+" no fue declarada")
+
+        except ValueError:
+            print(ValueError)
             # displaying the word
            # print(word)
        # for key in list(symbolTable.keys()):
@@ -46,6 +62,8 @@ def VerifySymbols():
 
         if symbolTable['('] == symbolTable[')']:
             return True
+        else:
+            return False
     else:
         return False
 # verifica si un string tiene formato de float
@@ -59,11 +77,53 @@ def VerifyFloat(potential_float):
 def VerifyString(word):
     firstChar = word[0]
     lastChar = word[-1]
-
     if firstChar =='“' and lastChar == '”':
+        return True
+    elif firstChar =="'" and lastChar == "'":
         return True
     else:
         return False
+
+def isFunction(anterior,posterior):
+    val = True if posterior == "(" else False
+    if anterior == "void" and val:
+        return True
+    elif anterior == "String" and val:
+        return True
+    elif anterior == "Float" and val:
+        return True
+    elif anterior == "int" and val:
+        return True
+    else:
+        return False
+
+def VerifyReturn(meth,word):
+    if word in typeVar and VerifySymbols():
+       if typeVar[meth]['Retorno'] == typeVar[word]['Retorno']:
+           return True
+       else:
+           print("El metodo retorna una varible de diferente tipo ")
+           return False
+    else:
+        return False
+        print("Error de sintaxis")
+
+def insertVar(word,valor):
+    if VerifySymbols():
+        varTable[word] = {'Tipo': 'Variable', 'Retorno': valor, 'Alcance': 'Global'}
+    else:
+        varTable[word] = {'Tipo': 'Variable', 'Retorno': valor, 'Alcance': 'Local'}
+def isVar(word):
+    yes = True
+    if word.isnumeric():
+        insertVar(word, "int")
+    elif VerifyFloat(word):
+        insertVar(word, "float")
+    elif VerifyString(word):
+        insertVar(word, "String")
+    else:
+        yes = False
+    return yes
 
 OpenFile()
 error = "No se encontró error de sintaxis " if VerifySymbols() else"Error de sintaxis"
